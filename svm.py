0498ea6fcbb6eaef31754bec1305ebe7f3e20cb0
ag6214 <ag6214@gmail.com>
2020-10-20 22:04:37 +0100

    cvm for anomaly detection

---
+++ svm.py
@@ -0,0 +1,27 @@
+# import libraries
+import pandas as pd
+from sklearn.svm import OneClassSVM
+import matplotlib.pyplot as plt
+from numpy import where
+
+#import data
+data = pd.read_csv("https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv")
+
+#input data
+df = data[["sepal_length","sepal_width"]]
+
+#model specification
+model = OneClassSVM(kernel = 'rbf' , gamma= 0.001, nu= 0.03).fit(df)
+
+#prediction
+y_pred = model.predict(df)
+
+#filter outlier index
+outlier_index = where(y_pred == -1)
+#filter outlier values
+outlier_values = df.iloc[outlier_index]
+
+#visualise outputs
+plt.scatter(data["sepal_length"],df["sepal_width"])
+plt.scatter(outlier_values["sepal_length"],outlier_values["sepal_width"], c = "r")
+plt.show()
